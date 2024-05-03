7cfa2ce8d6096ccc3a53b737dbc57d65fab33540
akanavaishnavi <168812514+akanavaishnavi@users.noreply.github.com>
2024-05-03 13:22:46 +0530

    Add files via upload

---
+++ Task-1/script.js
@@ -0,0 +1,5 @@
+const learnMoreButton = document.getElementById('learn-More');
+
+learnMoreButton.addEventListener('click', () => {
+  alert('Thanks for your interest in dogs! Visit our adoption page to find your perfect match!');
+});
